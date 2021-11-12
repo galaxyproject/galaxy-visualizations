8258b75d9483914147aae65638ea3cce1eb1388f
Dannon Baker <dannon.baker@gmail.com>
2021-11-12 13:43:50 -0500

    Expose more of utils at the primary charts index

--- packages/charts/src/index.js
+++ packages/charts/src/index.js
@@ -1,7 +1,15 @@
 // Just export everything from utilities for now.
 
-export { merge, getAjax, requestAjax } from "./utilities/utils";
-export { addZoom, makeTickFormat, makeUniqueCategories } from "./utilities/series";
+export { merge, getAjax, requestAjax } from "./utilities/utils.js";
+export {
+    addZoom,
+    getDomains,
+    makeCategories,
+    makeSeries,
+    makeTickFormat,
+    makeUniqueCategories,
+    mapCategories,
+} from "./utilities/series.js";
 
 // We have overlapping method names `request`, etc., in the following modules.  Assume direct access.
 //export * from "utilities/datasets";
