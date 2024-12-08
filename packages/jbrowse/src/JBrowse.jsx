3fefb64f812ea85cc191fa8c1543f96a8d63c167
guerler <aysam.guerler@gmail.com>
2024-12-08 11:56:52 +0300

    Run without rpcworker for now

--- packages/jbrowse/src/JBrowse.jsx
+++ packages/jbrowse/src/JBrowse.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import { createRoot, hydrateRoot } from "react-dom/client";
-//import { createViewState, JBrowseApp } from "@jbrowse/react-app";
 import { createViewState, JBrowseLinearGenomeView } from "@jbrowse/react-linear-genome-view";
 
 // @ts-expect-error no font types
@@ -10,9 +8,7 @@ export default function (props) {
     const [viewState, setViewState] = useState();
 
     useEffect(() => {
-        const state = createViewState({
-            ...props.config,
-        });
+        const state = createViewState(props.config);
         setViewState(state);
     }, []);
 
