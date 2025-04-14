cf457c97b39e8b65a1cfee73032fcedf2489d3cf
guerler <aysam.guerler@gmail.com>
2025-04-14 12:19:24 -0400

    Update config

--- packages/rerun/vite.config.js
+++ packages/rerun/vite.config.js
@@ -1,32 +1,9 @@
-import { searchForWorkspaceRoot } from "vite";
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import { viteConfigCharts } from "./vite.config.charts";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-/** @type {import("vite").UserConfig} */
-const config = {
-    // https://github.com/rerun-io/rerun/issues/6815
+export default defineConfig({
+    ...viteConfigCharts,
     optimizeDeps: {
         exclude: process.env.NODE_ENV === "production" ? [] : ["@rerun-io/web-viewer"],
     },
-    server: {
-        port: 5173,
-
-        fs: {
-            allow: [
-                searchForWorkspaceRoot(process.cwd()),
-                // NOTE: hack to allow `new URL("file://...")` in `web-viewer` when it is a linked package
-                fs.realpathSync(path.join(__dirname, "node_modules", "@rerun-io/web-viewer")),
-            ],
-        },
-    },
-};
-
-// Support deployment to GitHub Pages
-if ("REPOSITORY" in process.env) {
-    config.base = `/${process.env.REPOSITORY}/`;
-}
-
-export default config;
+});
--- packages/rerun/vite.config1.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import { viteConfigCharts } from "./vite.config.charts";
-
-export default defineConfig({
-    ...viteConfigCharts,
-});
