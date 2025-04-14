e95934f8a2654a92b293b0a6b2cee02f621a02c6
guerler <aysam.guerler@gmail.com>
2025-04-14 12:25:41 -0400

    Add galaxy snippet to access data

--- packages/rerun/src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-import { WebViewer } from "@rerun-io/web-viewer";
-
-const rrd = new URLSearchParams(location.search).get("url") || "motion.rrd";
-const viewer = new WebViewer();
-viewer.start(rrd, null, {
-    width: "100%",
-    height: "100%",
-});
