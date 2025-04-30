7266ad4dac47dc856c02172a248dc8d832011cdc
guerler <aysam.guerler@gmail.com>
2025-04-29 21:14:40 -0400

    Add alignment.js

---
+++ packages/alignment/main.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./main.css";
+import Alignment from "alignment.js/Alignment.js";
+
+const appElement = document.querySelector("#app");
+
+if (import.meta.env.DEV) {
+    const dataIncoming = {
+        root: "/",
+        visualization_config: {
+            dataset_id: process.env.dataset_id,
+        },
+    };
+    appElement.setAttribute("data-incoming", JSON.stringify(dataIncoming));
+}
+
+const incoming = JSON.parse(appElement?.getAttribute("data-incoming") || "{}");
+const datasetId = incoming.visualization_config.dataset_id;
+const root = incoming.root;
+const dataUrl = `${root}api/datasets/${datasetId}/display`;
+
+const messageElement = document.createElement("div");
+messageElement.id = "message";
+messageElement.style.display = "none";
+appElement.appendChild(messageElement);
+
+const viewerElement = document.createElement("div");
+viewerElement.id = "viewer";
+viewerElement.style.width = "100%";
+viewerElement.style.height = "100vh";
+appElement.appendChild(viewerElement);
+
+async function create() {
+    showMessage("Please wait...");
+    try {
+        const dataset = await getData(dataUrl);
+        ReactDOM.render(
+            <Alignment fasta={dataset} />,
+            viewerElement
+        );
+        hideMessage();
+    } catch (error) {
+        showMessage("Error loading dataset", error.message);
+    }
+}
+
+async function getData(url) {
+    try {
+        const { data } = await axios.get(url);
+        return data;
+    } catch (e) {
+        showMessage("Failed to retrieve data", e.message);
+        throw e;
+    }
+}
+
+function showMessage(title, details = null) {
+    details = details ? `: ${details}` : "";
+    messageElement.innerHTML = `<strong>${title}${details}</strong>`;
+    messageElement.style.display = "inline";
+    console.debug(`${title}${details}`);
+}
+
+function hideMessage() {
+    messageElement.style.display = "none";
+}
+
+create();
