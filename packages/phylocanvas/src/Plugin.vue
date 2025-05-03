e654bffc32a6bb616b16dc9533e1632a0f1304fb
guerler <aysam.guerler@gmail.com>
2025-05-03 13:21:57 -0400

    Add buttons to navigate

--- packages/phylocanvas/src/Plugin.vue
+++ packages/phylocanvas/src/Plugin.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ArrowPathIcon, ArrowLeftIcon, ArrowRightIcon } from "@heroicons/vue/24/outline";
+import { NAlert, NButton, NIcon, NTooltip } from "naive-ui";
 import { onMounted, ref, watch } from "vue";
 
 import phylocanvas from "@/phylocanvas.min.js";
@@ -15,6 +17,8 @@ const props = defineProps({
 let tree = null;
 
 const container = ref(null);
+const errorMessage = ref("");
+const isLoading = ref(true);
 const treeindex = ref(0);
 const treelist = ref([]);
 const viewport = ref(null);
@@ -34,21 +38,30 @@ function extract(content) {
             start = i + 1;
         }
     }
+    treelist.value.push(treelist.value[0]);
+    treelist.value.push(treelist.value[0]);
+    treelist.value.push(treelist.value[0]);
+    treelist.value.push(treelist.value[0]);
+    treelist.value.push(treelist.value[0]);
 }
 
 async function load() {
+    isLoading.value = true;
     const response = await fetch(props.datasetUrl);
     if (response.ok) {
         response.text().then((content) => {
             extract(content);
-            render();
+            render(0);
         });
     } else {
+        errorMessage.value = "Failed to load data.";
         console.error(response);
     }
+    isLoading.value = false;
 }
 
-async function render() {
+async function render(newIndex) {
+    treeindex.value = newIndex;
     if (treeindex.value >= 0 && treeindex.value < treelist.value.length) {
         const source = treelist.value[treeindex.value];
         if (tree) {
@@ -92,7 +105,42 @@ watch(
 </script>
 
 <template>
-    <div ref="viewport" class="h-screen overflow-hidden">
+    <n-alert v-if="errorMessage" type="error" class="m-1">
+        {{ errorMessage }}
+    </n-alert>
+    <div v-if="isLoading" class="m-2">
+        <ArrowPathIcon class="animate-spin size-4 inline mx-1" />
+        <span class="text-xs">Please wait...</span>
+    </div>
+    <div else ref="viewport" class="h-screen overflow-hidden">
         <div ref="container" class="h-screen"></div>
+        <div v-if="treelist.length > 1" class="absolute bottom-2 left-1/2 transform -translate-x-1/2">
+            <div class="flex items-center space-x-2">
+                <n-tooltip trigger="hover" :to="false">
+                    <template #trigger>
+                        <n-button size="small" :disabled="treeindex === 0" @click="render(treeindex - 1)">
+                            <template #icon>
+                                <n-icon><ArrowLeftIcon /></n-icon>
+                            </template>
+                        </n-button>
+                    </template>
+                    <span class="text-xs">Previous</span>
+                </n-tooltip>
+                <span class="text-sm font-medium"> {{ treeindex + 1 }} of {{ treelist.length }} </span>
+                <n-tooltip trigger="hover" :to="false">
+                    <template #trigger>
+                        <n-button
+                            size="small"
+                            :disabled="treeindex === treelist.length - 1"
+                            @click="render(treeindex + 1)">
+                            <template #icon>
+                                <n-icon><ArrowRightIcon /></n-icon>
+                            </template>
+                        </n-button>
+                    </template>
+                    <span class="text-xs">Next</span>
+                </n-tooltip>
+            </div>
+        </div>
     </div>
 </template>
