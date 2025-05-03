e06e874284b46cc8ae97f17c72bd446a660ad1a3
guerler <aysam.guerler@gmail.com>
2025-05-03 12:21:43 -0400

    Parse multiple trees

--- packages/phylocanvas/src/Plugin.vue
+++ packages/phylocanvas/src/Plugin.vue
@@ -15,41 +15,72 @@ const props = defineProps({
 let tree = null;
 
 const container = ref(null);
+const treeindex = ref(0);
+const treelist = ref([]);
 const viewport = ref(null);
 
-async function render() {
+function extract(content) {
+    treelist.value = [];
+    let depth = 0,
+        start = 0;
+    for (let i = 0; i < content.length; i++) {
+        const char = content[i];
+        if (char === "(") {
+            depth++;
+        } else if (char === ")") {
+            depth--;
+        } else if (char === ";" && depth === 0) {
+            treelist.value.push(content.slice(start, i + 1).trim());
+            start = i + 1;
+        }
+    }
+}
+
+async function load() {
     const response = await fetch(props.datasetUrl);
     if (response.ok) {
         response.text().then((content) => {
-            if (tree) {
-                tree.destroy();
-                tree = null;
-            }
-            tree = new phylocanvas(container.value, {
-                alignLabels: props.settings.align_labels,
-                fillColour: props.settings.node_color,
-                highlightColour: props.settings.highlighted_color,
-                interactive: true,
-                nodeShape: props.settings.node_shape,
-                source: content,
-                showLabels: props.settings.show_labels,
-                showLeafLabels: props.settings.show_labels,    
-                strokeColour: props.settings.edge_color,
-                type: props.settings.tree_type,
-            });
+            extract(content);
+            render();
         });
     } else {
         console.error(response);
     }
 }
 
+async function render() {
+    if (treeindex.value >= 0 && treeindex.value < treelist.value.length) {
+        const source = treelist.value[treeindex.value];
+        if (tree) {
+            tree.destroy();
+            tree = null;
+        }
+        tree = new phylocanvas(container.value, {
+            source,
+            alignLabels: props.settings.align_labels,
+            fillColour: props.settings.node_color,
+            highlightColour: props.settings.highlighted_color,
+            interactive: true,
+            nodeShape: props.settings.node_shape,
+            showLabels: props.settings.show_labels,
+            showLeafLabels: props.settings.show_labels,
+            strokeColour: props.settings.edge_color,
+            type: props.settings.tree_type,
+        });
+    } else {
+        console.error("no");
+    }
+}
+
 onMounted(() => {
-    render();
+    load();
 
     // resize tree on window resize
     window.addEventListener("resize", () => {
-        const bcr = viewport.value.getBoundingClientRect();
-        tree.resize(bcr.width, bcr.height);
+        if (tree) {
+            const bcr = viewport.value.getBoundingClientRect();
+            tree.resize(bcr.width, bcr.height);
+        }
     });
 });
 
