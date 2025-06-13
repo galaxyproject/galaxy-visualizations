0786435211807e81da418684ba7eaadfac0e15dd
guerler <aysam.guerler@gmail.com>
2025-06-13 13:11:53 -0400

    Fix path

--- packages/tiffviewer/vite.config.charts.js
+++ packages/tiffviewer/vite.config.charts.js
@@ -20,7 +20,7 @@ Object.keys(env).forEach((key) => {
 // https://vitejs.dev/config/
 export const viteConfigCharts = defineConfig({
   build: {
-    outDir: "./static/dist",
+    outDir: "./static",
     emptyOutDir: true,
     rollupOptions: {
       output: {
