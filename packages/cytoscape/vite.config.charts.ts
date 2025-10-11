4962873e2a079437fed02e7a6223dd09e371ac2c
guerler <aysam.guerler@gmail.com>
2025-10-11 11:16:32 +0300

    Update cytoscape, fix test

--- packages/cytoscape/vite.config.charts.ts
+++ packages/cytoscape/vite.config.charts.ts
@@ -6,6 +6,7 @@ const env = {
     GALAXY_ROOT: "http://127.0.0.1:8080",
 };
 
+// add history id for testing
 Object.keys(env).forEach((key) => {
     if (process.env[key]) {
         env[key] = process.env[key];
