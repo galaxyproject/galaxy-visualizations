c606cd8107f6fce473a279db9814d98c3ecd60de
guerler <aysam.guerler@gmail.com>
2025-10-12 20:25:09 +0300

    Avoid default playwright config if possible

--- .github/config/playwright.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@playwright/test";
-
-export default defineConfig({
-    timeout: 120000,
-    use: {
-        headless: !!process.env.CI,
-    },
-    snapshotPathTemplate: "{testDir}/test-data/{arg}.png",
-});
