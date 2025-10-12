f7de63f4619ce99f22bf7ca7b5ec86dea7888514
guerler <aysam.guerler@gmail.com>
2025-10-12 18:43:09 +0300

    No need to wait

--- packages/plotly/test.spec.js
+++ packages/plotly/test.spec.js
@@ -30,7 +30,6 @@ test("basic", async ({ page }) => {
 
     // start
     await page.goto("http://localhost:5173/");
-    await page.waitForTimeout(2000);
     await expect(page).toHaveScreenshot("0.png", { maxDiffPixelRatio: 0.02 });
     await page.click(".n-button");
     await expect(page).toHaveScreenshot("1.png", { maxDiffPixelRatio: 0.02 });
