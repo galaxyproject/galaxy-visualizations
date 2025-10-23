49d0652fe3f97ba4a7c174cffe6cfe615258a082
guerler <aysam.guerler@gmail.com>
2025-10-23 13:32:40 +0300

    Avoid using window.location in mvpapp

--- packages/mvpapp/src/js/publicPath.js
+++ packages/mvpapp/src/js/publicPath.js
@@ -1,3 +1,5 @@
-// Silly, but we don't have another mechanism to access this and using the whole path seems the best approach
+const appElement = document.querySelector("#app")
+const incoming = JSON.parse(appElement.dataset.incoming || "{}")
+
 const PLUGIN_PATH = "plugins/visualizations/mvpapp";
-__webpack_public_path__ = `${window.location.pathname.split(PLUGIN_PATH)[0]}static/${PLUGIN_PATH}/static/dist/`;
+__webpack_public_path__ = `${incoming.root}static/${PLUGIN_PATH}/static/`;
