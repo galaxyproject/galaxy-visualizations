4e0a22c40e8866c750c3d2f4a3bab6abee38e518
guerler <aysam.guerler@gmail.com>
2025-10-27 16:47:57 +0300

    Add tests for other plotly variants

--- packages/plotly/test.spec.js
+++ packages/plotly/test.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import DATASET_COLUMNS_MATRIX from "./test-data/api.datasets.columns.matrix.json" with { type: "json" };
 import DATASET_COLUMNS_NUMBER from "./test-data/api.datasets.columns.number.json" with { type: "json" };
 import DATASET_COLUMNS_TEXT_NUMBER from "./test-data/api.datasets.columns.text.number.json" with { type: "json" };
 import DATASET_DETAILS from "./test-data/api.datasets.details.json" with { type: "json" };
@@ -10,6 +11,14 @@ test("basic", async ({ page }) => {
             status: 200,
             contentType: "application/json",
             body: JSON.stringify(DATASET_DETAILS),
+        });
+    });
+
+    await page.route("**/api/datasets/__test__?data_type=raw_data&provider=dataset-column", async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: "application/json",
+            body: JSON.stringify(DATASET_COLUMNS_MATRIX),
         });
     });
 
@@ -42,11 +51,27 @@ test("basic", async ({ page }) => {
 
     // start
     await page.goto("http://localhost:5173/");
-    await expect(page).toHaveScreenshot("0.png", { maxDiffPixelRatio: 0.02 });
+    await expect(page).toHaveScreenshot("basic.png", { maxDiffPixelRatio: 0.02 });
     await page.click(".n-button");
-    await expect(page).toHaveScreenshot("1.png", { maxDiffPixelRatio: 0.02 });
+    await expect(page).toHaveScreenshot("basic_with_form.png", { maxDiffPixelRatio: 0.02 });
 
     // start pie
     await page.goto("http://localhost:5173?xml=plotly_pie");
-    await expect(page).toHaveScreenshot("2.png", { maxDiffPixelRatio: 0.02 });
+    await expect(page).toHaveScreenshot("pie.png", { maxDiffPixelRatio: 0.02 });
+
+    // start box
+    await page.goto("http://localhost:5173?xml=plotly_box");
+    await expect(page).toHaveScreenshot("box.png", { maxDiffPixelRatio: 0.02 });
+
+    // start histogram
+    await page.goto("http://localhost:5173?xml=plotly_histogram");
+    await expect(page).toHaveScreenshot("histogram.png", { maxDiffPixelRatio: 0.02 });
+
+    // start surface
+    await page.goto("http://localhost:5173?xml=plotly_surface");
+    await expect(page).toHaveScreenshot("surface.png", { maxDiffPixelRatio: 0.02 });
+
+    // start heatmap
+    await page.goto("http://localhost:5173?xml=plotly_heatmap");
+    await expect(page).toHaveScreenshot("heatmap.png", { maxDiffPixelRatio: 0.02 });
 });
