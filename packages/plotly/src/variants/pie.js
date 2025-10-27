b7ba17110f9dec566f32bf40ebf4b1450a941d4e
guerler <aysam.guerler@gmail.com>
2025-10-27 10:37:08 +0300

    Improve layout

--- packages/plotly/src/variants/pie.js
+++ packages/plotly/src/variants/pie.js
@@ -1,25 +1,36 @@
 import { useColumnsStore } from "galaxy-charts";
 
+const MARGIN = 30;
+const XGAP = 0.15;
+const YGAP = 0.25;
+
 const columnsStore = useColumnsStore();
 
 export default async function (datasetId, settings, tracks) {
     const columnsList = await columnsStore.fetchColumns(datasetId, tracks, ["labels", "values"]);
     const count = columnsList.length;
-    const side = Math.ceil(Math.sqrt(count));
-    const layout = { grid: { rows: side, columns: side } };
+    const columns = Math.ceil(Math.sqrt(count));
+    const rows = Math.ceil(count / columns);
+    const layout = {
+        grid: { rows, columns, xgap: XGAP, ygap: YGAP },
+        margin: { l: MARGIN, r: MARGIN, t: MARGIN, b: MARGIN },
+    };
     const config = { responsive: true };
     const data = [];
-    columnsList.forEach((columns, index) => {
+    columnsList.forEach((columnsData, index) => {
         const track = tracks[index];
-        const row = Math.floor(index / side);
-        const column = index % side;
+        const row = Math.floor(index / columns);
+        const column = index % columns;
         data.push({
             name: `${track.name} (${index + 1})`,
             type: "pie",
             hoverinfo: "label+percent",
+            textinfo: "percent",
+            textposition: "inside",
+            automargin: true,
             hole: settings.hole,
-            labels: columns.labels,
-            values: columns.values,
+            labels: columnsData.labels,
+            values: columnsData.values,
             domain: { row, column },
         });
     });
