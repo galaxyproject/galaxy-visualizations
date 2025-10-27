94c18eaaad26b7eac8e9f7200894185ac3d95aa0
guerler <aysam.guerler@gmail.com>
2025-10-27 10:31:56 +0300

    Allow adjusting hole size

--- packages/plotly/src/variants/pie.js
+++ packages/plotly/src/variants/pie.js
@@ -3,17 +3,24 @@ import { useColumnsStore } from "galaxy-charts";
 const columnsStore = useColumnsStore();
 
 export default async function (datasetId, settings, tracks) {
-    const layout = {};
-    const config = { responsive: true };
     const columnsList = await columnsStore.fetchColumns(datasetId, tracks, ["labels", "values"]);
+    const count = columnsList.length;
+    const side = Math.ceil(Math.sqrt(count));
+    const layout = { grid: { rows: side, columns: side } };
+    const config = { responsive: true };
     const data = [];
     columnsList.forEach((columns, index) => {
         const track = tracks[index];
+        const row = Math.floor(index / side);
+        const column = index % side;
         data.push({
             name: `${track.name} (${index + 1})`,
             type: "pie",
-            labels: columns.lables,
+            hoverinfo: "label+percent",
+            hole: settings.hole,
+            labels: columns.labels,
             values: columns.values,
+            domain: { row, column },
         });
     });
     return { data, layout, config };
