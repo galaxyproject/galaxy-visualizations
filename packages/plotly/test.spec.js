4c84679b4bc5780061fef5505b9bc2d285697d62
guerler <aysam.guerler@gmail.com>
2025-10-27 15:11:06 +0300

    Add basic plotly pie test

--- packages/plotly/test.spec.js
+++ packages/plotly/test.spec.js
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
-import DATASET_COLUMNS from "./test-data/api.datasets.columns.json" with { type: "json" };
+import DATASET_COLUMNS_NUMBER from "./test-data/api.datasets.columns.number.json" with { type: "json" };
+import DATASET_COLUMNS_TEXT_NUMBER from "./test-data/api.datasets.columns.text.number.json" with { type: "json" };
 import DATASET_DETAILS from "./test-data/api.datasets.details.json" with { type: "json" };
 
 test("basic", async ({ page }) => {
@@ -12,11 +13,19 @@ test("basic", async ({ page }) => {
         });
     });
 
+    await page.route("**/api/datasets/__test__?data_type=raw_data&provider=dataset-column&indeces=0%2C1", async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: "application/json",
+            body: JSON.stringify(DATASET_COLUMNS_TEXT_NUMBER),
+        });
+    });
+
     await page.route("**/api/datasets/__test__?data_type=raw_data&provider=dataset-column&indeces=1", async (route) => {
         await route.fulfill({
             status: 200,
             contentType: "application/json",
-            body: JSON.stringify(DATASET_COLUMNS),
+            body: JSON.stringify(DATASET_COLUMNS_NUMBER),
         });
     });
 
@@ -24,7 +33,7 @@ test("basic", async ({ page }) => {
         await route.fulfill({
             status: 200,
             contentType: "application/json",
-            body: JSON.stringify(DATASET_COLUMNS),
+            body: JSON.stringify(DATASET_COLUMNS_NUMBER),
         });
     });
 
@@ -33,4 +42,8 @@ test("basic", async ({ page }) => {
     await expect(page).toHaveScreenshot("0.png", { maxDiffPixelRatio: 0.02 });
     await page.click(".n-button");
     await expect(page).toHaveScreenshot("1.png", { maxDiffPixelRatio: 0.02 });
+
+    // start pie
+    await page.goto("http://localhost:5173?xml=plotly_pie");
+    await expect(page).toHaveScreenshot("2.png", { maxDiffPixelRatio: 0.02 });
 });
