714b7806bd89c72845c2a081505c67331cac12bb
guerler <aysam.guerler@gmail.com>
2025-10-27 11:13:28 +0300

    Add annotations

--- packages/plotly/src/variants/pie.js
+++ packages/plotly/src/variants/pie.js
@@ -1,8 +1,8 @@
 import { useColumnsStore } from "galaxy-charts";
 
-const MARGIN = 30;
-const XGAP = 0.15;
-const YGAP = 0.25;
+const MARGIN = 10;
+const XGAP = 0.1;
+const YGAP = 0.1;
 
 const columnsStore = useColumnsStore();
 
@@ -11,12 +11,16 @@ export default async function (datasetId, settings, tracks) {
     const count = columnsList.length;
     const columns = Math.ceil(Math.sqrt(count));
     const rows = Math.ceil(count / columns);
+    const annotations = [];
     const layout = {
         grid: { rows, columns, xgap: XGAP, ygap: YGAP },
         margin: { l: MARGIN, r: MARGIN, t: MARGIN, b: MARGIN },
+        annotations,
     };
     const config = { responsive: true };
     const data = [];
+    const xDomainWidth = (1 - (columns - 1) * XGAP / columns) / columns;
+    const yDomainHeight = (1 - (rows - 1) * YGAP / rows) / rows;
     columnsList.forEach((columnsData, index) => {
         const track = tracks[index];
         const row = Math.floor(index / columns);
@@ -32,6 +36,19 @@ export default async function (datasetId, settings, tracks) {
             labels: columnsData.labels,
             values: columnsData.values,
             domain: { row, column },
+        });
+        const x = (column + 0.5) * xDomainWidth + (column * XGAP) / columns;
+        const y = 1 - ((row + 0.5) * yDomainHeight + (row * YGAP) / rows);
+        annotations.push({
+            text: `<span style="background: red; padding: 4px 8px; border-radius: 6px;">${track.name}</span>`,
+            xref: "paper",
+            yref: "paper",
+            x,
+            y,
+            showarrow: false,
+            font: { size: 12 },
+            xanchor: "center",
+            yanchor: "top",
         });
     });
     return { data, layout, config };
