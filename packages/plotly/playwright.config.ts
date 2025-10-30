de9d783455b0e9b48d72dcb2c93538dfd64d22dc
guerler <aysam.guerler@gmail.com>
2025-10-30 16:08:01 +0300

    Remove launch options, needs to be revisited

--- packages/plotly/playwright.config.ts
+++ packages/plotly/playwright.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
     use: {
         headless: !!process.env.CI,
-        launchOptions: {
-            args: ["--font-render-hinting=none", "--disable-lcd-text"],
-        },
     },
     snapshotPathTemplate: "{testDir}/test-data/{arg}.png",
 });
