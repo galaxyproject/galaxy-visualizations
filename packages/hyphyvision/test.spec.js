9371a906829b966ca72c795a043c859866b2fdaa
guerler <aysam.guerler@gmail.com>
2025-10-30 15:48:13 +0300

    Update test, wait for dom

--- packages/hyphyvision/test.spec.js
+++ packages/hyphyvision/test.spec.js
@@ -4,5 +4,6 @@ const maxDiffPixelRatio = 0.03;
 
 test("basic", async ({ page }) => {
     await page.goto("http://localhost:8080/");
+    await page.waitForSelector("span:text('sequences in the alignment')");
     await expect(page).toHaveScreenshot("1.png", { maxDiffPixelRatio });
 });
