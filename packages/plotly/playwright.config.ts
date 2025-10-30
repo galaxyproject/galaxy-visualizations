18045d62eff50cb0b3f6c69d0853e5900b4430b2
guerler <aysam.guerler@gmail.com>
2025-10-30 15:56:19 +0300

    Add config

--- packages/plotly/playwright.config.ts
+++ packages/plotly/playwright.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
     use: {
-        headless: !!process.env.CI
+        headless: !!process.env.CI,
+        launchOptions: {
+            args: ["--font-render-hinting=none", "--disable-lcd-text"],
+        },
     },
-    snapshotPathTemplate: "{testDir}/test-data/{arg}.png"
+    snapshotPathTemplate: "{testDir}/test-data/{arg}.png",
 });
