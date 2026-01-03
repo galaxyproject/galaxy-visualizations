5a703a4ee466486f10ed673231ef66e3c90d3b22
guerler <aysam.guerler@gmail.com>
2026-01-03 18:51:20 +0300

    Collapse sidepanel on initalization

--- packages/igv/src/main.js
+++ packages/igv/src/main.js
@@ -51,7 +51,7 @@ async function main() {
      * and passing in any necessary props such as credentials.
      */
     createApp({
-        render: () => h(App, { container: container, credentials: process.env.credentials }),
+        render: () => h(App, { collapse: true, container: container, credentials: process.env.credentials }),
     }).mount(`#${container}`);
 }
 
