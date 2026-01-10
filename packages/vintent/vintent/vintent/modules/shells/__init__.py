aa33e247441a50b77976527d4c04afa2aa9049ee
guerler <aysam.guerler@gmail.com>
2026-01-11 00:11:38 +0300

    Add covariance shell

--- packages/vintent/vintent/vintent/modules/shells/__init__.py
+++ packages/vintent/vintent/vintent/modules/shells/__init__.py
@@ -4,6 +4,7 @@ from .box_plot import BoxPlotShell
 from .cardinality_report import CardinalityReportShell
 from .density import DensityShell
 from .heatmap_correlation import HeatmapCorrelationShell
+from .heatmap_covariance import HeatmapCovarianceShell
 from .histogram import HistogramShell
 from .line_time import LineTimeShell
 from .linear_regression import LinearRegressionShell
@@ -19,6 +20,7 @@ SHELLS = {
     "density": DensityShell(),
     "linear_regression": LinearRegressionShell(),
     "heatmap_correlation": HeatmapCorrelationShell(),
+    "heatmap_covariance": HeatmapCovarianceShell(),
     "histogram": HistogramShell(),
     "line_time": LineTimeShell(),
     "pca": PCAShell(),
