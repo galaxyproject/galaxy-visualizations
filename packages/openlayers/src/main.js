06cba8d832928432fc95ef00cf80d46fbf783f34
guerler <aysam.guerler@gmail.com>
2026-04-09 12:51:56 +0300

    Update to latest template, collapse side panel

--- packages/openlayers/src/main.js
+++ packages/openlayers/src/main.js
@@ -40,7 +40,7 @@ async function main() {
      * and passing in any necessary props such as credentials.
      */
     createApp({
-        render: () => h(App, { credentials: process.env.credentials }),
+        render: () => h(App, { collapse: true, credentials: process.env.credentials }),
     }).mount("#app");
 }
 
