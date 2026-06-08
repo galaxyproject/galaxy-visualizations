05693a8259b62ae3e2c8520ac5c6b3cc21e41922
Björn Grüning <bjoern@gruenings.eu>
2026-06-08 14:41:17 +0200

    Remove launchOptions from Playwright config
    Removed launchOptions for headless Chromium.

--- packages/jsonviewer/playwright.config.ts
+++ packages/jsonviewer/playwright.config.ts
@@ -6,10 +6,6 @@ export default defineConfig({
     timeout: 60000,
     use: {
         headless: true,
-        launchOptions: {
-            executablePath: "/snap/bin/chromium",
-            args: ["--no-sandbox", "--disable-gpu"],
-        },
     },
     webServer: {
         command: "GALAXY_DATASET_ID=__test__ npm run dev",
