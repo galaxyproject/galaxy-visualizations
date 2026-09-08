1c5f25f8069f1b7bab2f0eeec055ae4cb4d47624
guerler <aysam.guerler@gmail.com>
2026-09-08 13:13:39 +0300

    Restore headless ci optional

--- packages/jupyterlite/playwright.config.ts
+++ packages/jupyterlite/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
     timeout: 120000, // 120 seconds per test
     use: {
-        headless: true, //!!process.env.CI,
+        headless: !!process.env.CI,
     },
     webServer: {
         command: "npm run dev",
